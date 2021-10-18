5824c9fa2486489c64e3fe84bebdfe7d692136cb
Sorin Mihai Toma <sorin.toma@softvision.com>
2021-10-18 16:00:51 +0300

    copy to clipboard button - added user feedback

--- ui/shared/Clipboard.jsx
+++ ui/shared/Clipboard.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClipboard } from '@fortawesome/free-regular-svg-icons';
 import { Button } from 'reactstrap';
 
 const Clipboard = ({ description, text, outline, visible, color }) => {
+  const [copied, setCopied] = useState(false);
+
   if (!text) {
     return null;
   }
 
-  const copyToClipboard = () => navigator.clipboard.writeText(text);
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 700);
+  };
 
   return (
     <Button
       type="button"
       title={`Copy ${description}`}
       onClick={copyToClipboard}
-      className={`py-0 px-1 ${visible ? '' : 'invisible'}`}
+      className="py-0 px-1"
       color={`${color || 'light'}`}
       outline={outline}
     >
-      <FontAwesomeIcon icon={faClipboard} />
+      {copied ? (
+        <div className="copy-to-clipboard">{description} copied</div>
+      ) : (
+        <FontAwesomeIcon icon={faClipboard} />
+      )}
     </Button>
   );
 };
